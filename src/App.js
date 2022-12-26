b0cbdbd53994e79f358f42d920434c65d4130f1f
LarryTheFatCat@users.noreply.github.com <83801755+LarryTheFatCat@users.noreply.github.com>
2022-12-25 20:20:38 -0500

    remove useless files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const something = () => {
+    console.log("hi mo");
+  }
   return (
     <div className="container">
-
     </div>
   );
 }
